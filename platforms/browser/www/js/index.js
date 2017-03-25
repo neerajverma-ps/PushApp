3bd65d9402f9ec670e643903a0fbed82fb33f0f9
Neeraj Verma <nverma5@sapient.com>
2017-03-25 02:01:03 +0000

    geo location code included

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -36,6 +36,7 @@ var app = {
         console.log('Received Device Ready Event');
         console.log('calling setup push');
         app.setupPush();
+		app.getGeoLoc();
     },
     setupPush: function() {
         console.log('calling push init');
@@ -84,5 +85,30 @@ var app = {
                 'Yo Man!'                  // buttonName
             );
        });
-    }
+    },
+	getGeoLoc: function() {
+		// onSuccess Callback
+		// This method accepts a Position object, which contains the
+		// current GPS coordinates
+		//
+		var onSuccess = function(position) {
+			alert('Latitude: '          + position.coords.latitude          + '\n' +
+				  'Longitude: '         + position.coords.longitude         + '\n' +
+				  'Altitude: '          + position.coords.altitude          + '\n' +
+				  'Accuracy: '          + position.coords.accuracy          + '\n' +
+				  'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+				  'Heading: '           + position.coords.heading           + '\n' +
+				  'Speed: '             + position.coords.speed             + '\n' +
+				  'Timestamp: '         + position.timestamp                + '\n');
+		};
+
+		// onError Callback receives a PositionError object
+		//
+		function onError(error) {
+			alert('code: '    + error.code    + '\n' +
+				  'message: ' + error.message + '\n');
+		}
+		
+		navigator.geolocation.getCurrentPosition(onSuccess, onError);
+	}
 };
